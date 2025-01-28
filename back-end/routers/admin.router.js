897ef3ff117e41782406641ca49c149dcc6a450e
BochraBenRomdhane <bochrabenromdhane2003@gmail.com>
2025-01-28 20:59:00 +0100

    bochra

--- back-end/routers/admin.router.js
+++ back-end/routers/admin.router.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const RouterAdmin = express.Router();
-const { login, register } = require("../controllers/admin.controler.js")
+const { login, register } = require("../controllers/admin.controller")
 
 RouterAdmin.post("/login", login);
 RouterAdmin.post("/register", register);
