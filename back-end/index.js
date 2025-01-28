ed0e13dfeaf6c5bd355f4425f7b92e45d2e42f7f
khaled-akrout <khaledakrout23@gmail.com>
2025-01-28 13:18:00 +0100

    v1

--- back-end/index.js
+++ back-end/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const App = express();
 const port = 5000;
 const cors=require("cors")
+console.log("hello");
 
 const userRoute=require("./routers/user.router")
 
