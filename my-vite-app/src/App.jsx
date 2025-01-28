ef0209e23382d19ba49ea61d5ac5f6584299a389
Adem-Maafi <ademmaafi3@gmail.com>
2025-01-28 15:46:51 +0100

    login done from adem

--- my-vite-app/src/App.jsx
+++ my-vite-app/src/App.jsx
@@ -1,19 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React ,{ useState } from 'react'
+import SingUpUser from './componet/singUpUser'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const App =()=> {
+  
 
   return (
-    <>
+    <div>
       
         
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+
+      <SingUpUser/>
+    </div>
+   
   )
 }
 
---
+++ my-vite-app/src/componet/loginUser.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function loginUser() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default loginUser
