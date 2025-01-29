f06d6c34b9691ecd41a59ac795094bdaf459eed5
khaled-akrout <khaledakrout23@gmail.com>
2025-01-29 10:11:31 +0100

    v2

--- my-vite-app/src/store/index.js
+++ my-vite-app/src/store/index.js
@@ -1,13 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-// import todosReducer from '../features/todos/todosSlice'
-// import filtersReducer from '../features/filters/filtersSlice'
-// import counterReducer from './reducers/counter'
 import UserReducer from "../store/reducers/user"
 const store = configureStore({
   reducer: {
- counter:counterReducer,
- category:categoryReducer,
- auth: authReducer,
+ 
  user:UserReducer,
 
   }
