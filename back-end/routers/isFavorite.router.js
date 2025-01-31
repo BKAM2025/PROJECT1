678d6b67a757c8f3f6ff1993393590d87cf2f69f
Adem-Maafi <ademmaafi3@gmail.com>
2025-01-31 16:27:09 +0100

    update

--- back-end/routers/isFavorite.router.js
+++ back-end/routers/isFavorite.router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { toggleFavorite, getFavoriteProducts } = require("../controllers/isFavorite.controller");
 const refreshTokenMiddleware =require("../middlewares/auth")
 
-router.put("/:productId/favorite",refreshTokenMiddleware, toggleFavorite);
-router.get("/favorites", getFavoriteProducts);
+router.put("/:productId",refreshTokenMiddleware, toggleFavorite);
+router.get("/favorites",refreshTokenMiddleware, getFavoriteProducts);
 
 module.exports = router;
